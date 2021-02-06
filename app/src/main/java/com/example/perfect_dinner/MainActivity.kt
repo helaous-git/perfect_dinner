040e2c27695384e1758e258f0471c24ccfa95b77
slim <helaouislim@gmail.com>
2021-02-06 21:18:19 +0100

    Add a splash screen at the start of the activity. Need to change the image latter

--- app/src/main/java/com/example/perfect_dinner/MainActivity.kt
+++ app/src/main/java/com/example/perfect_dinner/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+    // go back to original theme after splash screen
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }

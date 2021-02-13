b33213923788733a8289b3dd3f392414274f6ccb
slim <helaouislim@gmail.com>
2021-02-13 15:39:37 +0100

    design of the menu with a recyclerview and adapter model for the data

--- app/src/main/java/com/example/perfect_dinner/activities/MainActivity.kt
+++ app/src/main/java/com/example/perfect_dinner/activities/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.perfect_dinner.activities
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.perfect_dinner.R
-import com.example.perfect_dinner.firebase.FireStoreClass
 import com.example.perfect_dinner.firebase.FirebaseFunctionActivity
+import com.example.perfect_dinner.models.BetInfo
+import com.example.perfect_dinner.recyclerviewadapter.ItemAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.menu_recycler_view.*
 
 class MainActivity : FirebaseFunctionActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +16,32 @@ class MainActivity : FirebaseFunctionActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createSignInIntent()
+        listView()
+    }
+
+    private fun listView() {
+
+        // Set the LayoutManager that this RecyclerView will use.
+        rvItemsList.layoutManager = LinearLayoutManager(this)
+        // Adapter class is initialized and list is passed in the param.
+        val itemAdapter = ItemAdapter(this, getItemsList())
+        // adapter instance is set to the recyclerview to inflate the items.
+        rvItemsList.adapter = itemAdapter
+    }
+
+    private fun getItemsList(): ArrayList<BetInfo> {
+
+        val list = ArrayList<BetInfo>()
+        var betInfo= BetInfo("1","12 Jan","5","32","De la cuisine de chine","Monaco"," 20 €")
+
+        list.add(betInfo)
+        list.add(betInfo)
+        list.add(betInfo)
+        list.add(betInfo)
+        list.add(betInfo)
+        list.add(betInfo)
+
+
+        return list
     }
 }
--- app/src/main/java/com/example/perfect_dinner/utils/Constants.kt
+++ app/src/main/java/com/example/perfect_dinner/utils/Constants.kt
@@ -3,4 +3,7 @@ package com.example.perfect_dinner.utils
 object Constants {
     const val USERS : String = "users"
     const val RC_SIGN_IN = 123
+    const val LIST_VIEW = "LIST_VIEW" // Value for List View
+    const val GRID_VIEW = "GRID_VIEW"
+
 }
